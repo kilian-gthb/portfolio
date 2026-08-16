930374901a4ae594bea829d0bb3c01bbce5f2f4c
kilian-gthb <186335278+kilian-gthb@users.noreply.github.com>
2026-08-16 19:03:59 +0200

    upgrade style

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
-
     let btn = document.getElementById("theme-btn");
     if (document.body.classList.contains("dark-mode")) {
         btn.textContent = "Passer au thème clair";
